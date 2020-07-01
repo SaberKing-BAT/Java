323422c268ad8b05f1c08dc0838879e0047d27c6
SaberKing-BAT <2206987800@qq.com>
2020-07-01 19:01:44 +0800

    ..

---
+++ java基础代码库/src/LYC/循环语句/IfElse.java
@@ -0,0 +1,14 @@
+package LYC.循环语句;
+
+public class IfElse {
+    public static void test1(){
+        int n=1;
+        if (n==1)
+            System.out.println(n);
+        else
+            System.out.println(n);
+    }
+    public static void main(String[] args) {
+        test1();
+    }
+}
